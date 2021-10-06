bbed55eef90204a34d42612098b0220e69989696
Michael Whatcott <mdwhatcott@gmail.com>
2021-10-05 21:33:51 -0600

    Implemented the algorithms.

--- main.go
+++ main.go
@@ -1,1 +1,40 @@
-package simple_sort
+// Package simplesort implements the algorithms described here:
+// https://arxiv.org/pdf/2110.01111.pdf
+//
+// Also see discussion here:
+// https://news.ycombinator.com/item?id=28758106
+//
+package simplesort
+
+func Algorithm1(A []int) {
+	n := len(A)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if A[i] < A[j] {
+				A[i], A[j] = A[j], A[i]
+			}
+		}
+	}
+}
+
+func Algorithm2(A []int) {
+	n := len(A)
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			if A[i] > A[j] {
+				A[i], A[j] = A[j], A[i]
+			}
+		}
+	}
+}
+
+func Algorithm3(A []int) {
+	n := len(A)
+	for i := 0; i < n; i++ {
+		for j := 1; j < i-1; j++ {
+			if A[i] < A[j] {
+				A[i], A[j] = A[j], A[i]
+			}
+		}
+	}
+}
---
+++ main_test.go
@@ -0,0 +1,58 @@
+package simplesort
+
+import "testing"
+
+func assertSort(t *testing.T, algorithm func([]int)) {
+	var (
+		initial  = []int{5, 3, 1, 8, 10, 2, 0}
+		expected = []int{0, 1, 2, 3, 5, 8, 10}
+	)
+
+	algorithm(initial)
+
+	if !equal(initial, expected) {
+		t.Errorf("\n"+
+			"Expected: %v\n"+
+			"Actual:   %v", expected, initial)
+	}
+}
+
+func equal(actual, expected []int) bool {
+	if len(actual) != len(expected) {
+		return false
+	}
+	for i := range actual {
+		if actual[i] != expected[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func Test1(t *testing.T) {
+	assertSort(t, Algorithm1)
+}
+func Test2(t *testing.T) {
+	assertSort(t, Algorithm2)
+}
+func Test3(t *testing.T) {
+	t.Skip("hmm, this one's not working as advertised...")
+	assertSort(t, Algorithm3)
+}
+
+func Benchmark1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Algorithm1([]int{5, 3, 1, 8, 10, 2, 0})
+	}
+}
+func Benchmark2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Algorithm2([]int{5, 3, 1, 8, 10, 2, 0})
+	}
+}
+func Benchmark3(b *testing.B) {
+	b.Skip("not working")
+	for i := 0; i < b.N; i++ {
+		Algorithm3([]int{5, 3, 1, 8, 10, 2, 0})
+	}
+}
